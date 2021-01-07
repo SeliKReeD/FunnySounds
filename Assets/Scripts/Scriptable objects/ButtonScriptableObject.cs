65711e33ef482b92825cc5f2e19b9e19edcea285
Seliverstov Vladyslav <seliverstovvladislavpb@gmail.com>
2021-01-07 17:41:20 +0300

    add scriptable object for buttons

---
+++ Assets/Scripts/Scriptable objects/ButtonScriptableObject.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonScriptableObject : ScriptableObject
+{
+    public int id;
+
+    public AudioClip Clip;
+    public Sprite sprite;
+}
---
+++ Assets/Scripts/Scriptable objects/MakeButtonScriptableObject.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class MakeButtonScriptableObject
+{
+    [MenuItem("Assets/Create/Button Object")]
+    public static void CreateMyAsset()
+    {
+        ButtonScriptableObject buttonAsset = ScriptableObject.CreateInstance<ButtonScriptableObject>();
+
+        AssetDatabase.CreateAsset(buttonAsset, "Assets/NewScriptableObject.asset");
+        AssetDatabase.SaveAssets();
+
+        EditorUtility.FocusProjectWindow();
+
+        Selection.activeObject = buttonAsset;
+    }
+}
