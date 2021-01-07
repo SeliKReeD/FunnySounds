99bb920a5dc5ba630fcd302954712be0dcbb5a01
Seliverstov Vladyslav <seliverstovvladislavpb@gmail.com>
2021-01-07 15:36:05 +0300

    project start

---
+++ Assets/Scripts/ButtonController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonController : MonoBehaviour
+{
+
+    private int id;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.GetComponent<Button>().onClick.AddListener(OnButtonClick);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnButtonClick()
+    {
+        Debug.Log("Button click " + id);
+    }
+
+    public void setId(int id)
+    {
+        this.id = id;
+    }
+
+    public int getId()
+    {
+        return id;
+    }
+}
---
+++ Assets/Scripts/MainController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class MainController : MonoBehaviour
+{
+    public Transform buttonPanel;
+    public GameObject buttonPrefab;
+    public int buttonCount;
+    public int columns;
+
+    public GridLayoutGroup gridLayoutGroup;
+    // Start is called before the first frame update
+    void Start()
+    {
+        float width = this.GetComponent<RectTransform>().rect.width;
+        Debug.Log(width);
+        gridLayoutGroup.cellSize = new Vector2(width/columns, width/columns);
+
+        for (int i = 0; i < buttonCount; i++)
+        {
+            GameObject newButton = (GameObject) Instantiate(buttonPrefab);
+            newButton.GetComponent<ButtonController>().setId(i);
+            newButton.transform.SetParent(buttonPanel, false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
